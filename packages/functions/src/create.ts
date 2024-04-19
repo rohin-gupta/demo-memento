af89d326251e82cc8f9d7fbc7c43d67c63f50a0b
rohin-gupta <gupt1011@purdue.edu>
2024-04-19 01:17:20 -0400

    Added the create API

---
+++ packages/functions/src/create.ts
@@ -0,0 +1,56 @@
+import AWS from "aws-sdk";
+import * as uuid from "uuid";
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+import { Table } from "sst/node/table";
+import { stat } from "fs";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event: APIGatewayProxyEvent) {
+    let data, params;
+
+    //Body of request passed in as a JSON encoded string in "event.body"
+    if(event.body) {
+        data = JSON.parse(event.body);
+        params = {
+            TableName: Table.Notes.tableName,
+            Item: {
+                //Attributes of the item to be created
+                userID: "123", //The ID of the author
+                noteID: uuid.v1(), //The unique uuid
+                content: data.content, //Parsed from the request body
+                attachment: data.attachment, //Parsed from the request body
+                createdAt: Date.now(), //Current date
+            },
+        };
+    }
+    else {
+        return {
+            statusCode: 404,
+            body: JSON.stringify({ error: true }),
+        };
+    }
+
+    try {
+        await dynamoDB.put(params).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(params.Item),
+        };
+    }
+    catch (error) {
+        let message;
+        if (error instanceof Error) {
+            message = error.message;
+        }
+        else {
+            message = String(error);
+        }
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: message }),
+        };
+    }
+}
