1e26a35e9a1219b813920356005ea3329a6060b3
rohin-gupta <gupt1011@purdue.edu>
2024-06-23 03:24:05 -0400

    setting up the frontend

---
+++ packages/frontend/src/containers/NotFound.tsx
@@ -0,0 +1,9 @@
+import "./NotFound.css";
+
+export default function NotFound() {
+    return (
+        <div className="NotFound text-center">
+            <h3>Sorry, the requested page doesn't exist!</h3>
+        </div>
+    );
+}
